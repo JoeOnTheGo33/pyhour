095abce2f8406c36dd0cab674520813912c64e88
JoeOnTheGo33 <joeonthego100@gmail.com>
2022-03-07 17:41:06 -0500

    reamp: required hours override & better data process

--- tally.py
+++ tally.py
@@ -7,19 +7,29 @@ from datetime import datetime, timedelta
 from datetime import date
 from dateutil.rrule import rrule, DAILY
 import dateutil.relativedelta as rel
+import io
+
+REQ = """Date,Required
+2022-02-21,0
+2022-02-22,0
+"""
+HOURS_PER_DAY = 7
 
 
 def nice(v):
-    return "%d:%02.0f" % (int(v), int((v % 1) * 60))
+    if v < 0:
+        return ("-%d:%02.0f" % (int(abs(v)), int((abs(v) % 1) * 60))).rjust(6)
+    else:
+        return ("%d:%02.0f" % (int(abs(v)), int((abs(v) % 1) * 60))).rjust(6)
 
 
 if __name__ == "__main__":
     WEEK = datetime.today().isocalendar()[1]
-    print("WEEK", WEEK)
+    print(" " * 12 + "===", "WEEK", WEEK, "===")
     log = pd.read_csv("/home/jy/Me/pyhour/w4.hours", delimiter=',',
                       quotechar='"', parse_dates=[['Date', 'Time']],
                       date_parser=lambda x, y: pd.to_datetime(x + " " + y, format='%y/%m/%d %H:%M'))
-    print(log.tail())
+    print("RECENT LOG:"); print(log.tail())
     tally = pd.DataFrame(columns=["Date", "Hours"])
 
     start = log.iloc[0]["Date_Time"]
@@ -27,7 +37,8 @@ if __name__ == "__main__":
     h = 0.0
 
     print()
-    print("Start at:", start)
+    print("  Start at:", start)
+    print()
 
     for i, r in log.iloc[1:, :].iterrows():
         end = r["Date_Time"]
@@ -51,33 +62,61 @@ if __name__ == "__main__":
     tally = pd.concat([tally, pd.DataFrame({"Date": [start.date()], "Hours": [h]})],
                       ignore_index=True, axis=0)
 
+    tally["Date"] = pd.to_datetime(tally["Date"])
+
+    req_override = pd.read_csv(
+        io.StringIO(REQ),
+        sep=",",
+        parse_dates=['Date'])
+    res = pd.merge(tally, req_override, on="Date", how="outer")
+    res.loc[~res["Required"].notna(), "Required"] = HOURS_PER_DAY
+    res["Required"] = pd.to_numeric(res["Required"])
+    res.loc[~res["Hours"].notna(), "Hours"] = 0
+    res["Hours"] = pd.to_numeric(res["Hours"])
+    tally = res
+
     tally["Week"] = tally["Date"].apply(lambda x: x.isocalendar()[1])
+    tally = tally.sort_values("Date")
 
-    weekly_tally = tally.groupby("Week").sum()
-    weekly_tally["Required"] = 35
-    weekly_tally.at[1, "Required"] = 8 * 4
-    weekly_tally["Diff"] = weekly_tally["Required"] - weekly_tally["Hours"]
-
-    print()
+    print("RECENT TALLY:")
+    overtime = tally.loc[tally["Hours"] > 10]
+    if overtime.shape[0] > 0:
+        print("    OVERTIME ERROR !!")
+        print(overtime)
+        quit()
     print(tally.tail())
     tally.to_csv("tally.log", index=False)
-    print()
-    print(weekly_tally.tail())
+
+    weekly_tally = tally.groupby("Week").sum()
+    overtime = weekly_tally["Required"] > 35
+    weekly_tally.loc[overtime, "Required"] = HOURS_PER_DAY * 5
 
     H = 0
-    rd = rel.relativedelta(days=1, weekday=rel.FR)
-    next_friday = date.today() + rd
-    print()
-    print("Days left in week:")
-    for dt in rrule(DAILY, dtstart=date.today() + timedelta(days=1), until=next_friday):
-        H += 8
-        print(dt.strftime("\t%Y-%m-%d"))
+    if date.today().weekday() != 4:
+        rd = rel.relativedelta(days=1, weekday=rel.FR)
+        next_friday = date.today() + rd
+        print()
+        print("Days left in week:")
+        for dt in rrule(DAILY, dtstart=date.today() + timedelta(days=1), until=next_friday):
+            H += HOURS_PER_DAY
+            print(dt.strftime("\t%Y-%m-%d"))
+        weekly_tally.at[weekly_tally.shape[0], "Required"] += H
+    else:
+        print()
+        print("Happy Friday!")
+
+    weekly_tally["Diff"] = weekly_tally["Required"] - weekly_tally["Hours"]
+
+    print(); print("WEEKLY TALLY:"); print(weekly_tally.tail())
 
     HD = weekly_tally["Diff"].sum()
 
     print()
     print("RECENT:")
-    print("\tHours required this week:\t", nice(weekly_tally.iloc[-1]["Diff"]))
-    print("ACCUMLATED:")
-    print("\tHours required this week:\t", nice(HD))
-    print("\tHours required today:\t\t", nice(HD - H))
+    print("    ", "This week: ".ljust(10), nice(weekly_tally.iloc[-1]["Diff"]), "    ",
+          "Today: ".ljust(7), nice(tally.iloc[-1]["Required"] - tally.iloc[-1]["Hours"]),
+          sep="")
+    print("ACCUMULATED:")
+    print("    ", "This week: ".ljust(10), nice(HD), "    ",
+          "Today: ".ljust(7), nice(HD - H),
+          sep="")
